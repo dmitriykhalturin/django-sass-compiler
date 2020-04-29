5cf2647230404279f49962429d70c8c80ea280fc
Jacek B. Budzynski <jacek.budzynski@fsinfo.es>
2020-04-29 22:32:33 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+import setuptools
+
+with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf8") as readme:
+    README = readme.read()
+
+setuptools.setup(
+    name="django-sass-compiler",
+    version="1.0.0",
+    author="Jacek B. Budzynski",
+    author_email="jacek.b.budzynski@gmail.com",
+    url="https://github.com/jaberbu/django-sass-compiler",
+    description="Using libsass, compile all .scss files found in the paths defined in settings.STATICFILES_FINDERS.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="BSD license",
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "django",
+        "libsass"
+    ],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: Spanish",
+        "Natural Language :: Polish",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Framework :: Django",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 3.0",
+        "Environment :: Web Environment",
+    ],
+)
